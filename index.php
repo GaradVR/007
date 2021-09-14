c29d7efa6ba0b06e03f644ad98c5896498567f8a
GaradVR <90440360+GaradVR@users.noreply.github.com>
2021-09-14 16:19:35 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,40 @@
+<!doctype>
+<html>
+ <Head>
+ <link rel="stylesheet" href="master.css">
+ <body style="
+    background:#333;
+	background-image: url('https://images8.alphacoders.com/371/371663.jpg');
+">
+ <header>
+ <div class="logo">
+	 <img src="../images//logo.png" width="250" height="150" class="logo-img">
+	 <span class="logo-title"><b></b></span>
+   </div>
+ <div class="hor-menu">
+ <ul>
+	 <li><a href=""><h2>+7(351)111-22-33</h2></a></li>
+	 <li><a href=""><img src="../images//call.jpg" width="46" height="44" class="img"></a></li>
+ </ul>
+ </div>
+
+
+ </header>
+ <div class="wrapper">
+
+ <div class="content">
+
+</div>
+ </div>
+ <div class="sidebar">
+ 
+ </div>
+ <div class="slides">
+	 <div>  </div>
+	 <div>  </div>
+	 <div>  </div>
+	 <div>  </div>
+	 </div>
+	   
+	   </body>
+	   </html>
